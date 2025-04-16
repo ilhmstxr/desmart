7bb8483208e76c105ee34462b859e9874d59ef74
ilhmstxr <ilhambintanghuh30332@gmail.com>
2025-04-17 06:15:28 +0700

    mod: login

--- main/authenticate/login.php
+++ main/authenticate/login.php
@@ -2,18 +2,26 @@
 include '../koneksi.php';
 
 session_start();
-// simpan data u ser
-$_SESSION['userId'] = $user['id'];
-
-
 
 if (isset($_POST['login'])) {
     $email = $_POST['email'];
     $password = $_POST['password'];
 
-    $query = "SELECT * FROM user WHERE email = '$email' AND password = '$password'";
+    // ambil data user berdasarkan email
+    $query = "SELECT * FROM users WHERE email = '$email'";
     $result = mysqli_query($koneksi, $query);
-    if ($result) {
-        header("Location: ../dashboard.php");
+
+    if ($result && mysqli_num_rows($result) > 0) {
+        $user = mysqli_fetch_assoc($result);
+
+        // jika password tidak di-hash, langsung bandingkan:
+        if ($password === $user['password']) {
+            $_SESSION['userId'] = $user['id']; // GUNAKAN nama kolom sebenarnya
+            header("Location: ../Dashboard/01_Dashboard.html");
+        } else {
+            echo "<script>alert('Email atau password salah!'); window.location.href='login.html';</script>";
+        }
+    } else {
+        echo false; // email tidak ditemukan
     }
 }
